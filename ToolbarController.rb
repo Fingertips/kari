0e53eef4a116fb267e68fdf45f13964bd7744647
eloy <eloy@7a99d2f0-d533-0410-96fe-f25cb2755c1e>
2007-07-02 14:12:12 +0000

    Added the custom view that holds the font size smaller and bigger buttons to the toolbar.
    git-svn-id: https://fngtps.com/svn/kari/trunk/Kari_osx_app/Kari@15 7a99d2f0-d533-0410-96fe-f25cb2755c1e

--- ToolbarController.rb
+++ ToolbarController.rb
@@ -24,7 +24,7 @@ class ToolbarController < OSX::NSObject
   end
   
   #TOOLBAR_ITEMS = ['HistoryBackAndForwardItem', 'FontSmallerAndBiggerItem', OSX::NSToolbarFlexibleSpaceItemIdentifier, 'SearchItem']
-  TOOLBAR_ITEMS = ['HistoryBackAndForwardItem', OSX::NSToolbarFlexibleSpaceItemIdentifier]
+  TOOLBAR_ITEMS = ['HistoryBackAndForwardItem', 'FontSmallerAndBiggerItem', OSX::NSToolbarFlexibleSpaceItemIdentifier]
   def toolbarAllowedItemIdentifiers(toolbar)
     TOOLBAR_ITEMS
   end
