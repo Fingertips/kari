5362f9a946ad8d2c4bf244d30eb8efb0db9df097
eloy <eloy@7a99d2f0-d533-0410-96fe-f25cb2755c1e>
2007-07-02 13:37:51 +0000

    Added the ToolbarController and it's specs. Right now it only adds a "spacer" item, so nothing is visible yet.
    git-svn-id: https://fngtps.com/svn/kari/trunk/Kari_osx_app/Kari@13 7a99d2f0-d533-0410-96fe-f25cb2755c1e

---
+++ ToolbarController.rb
@@ -0,0 +1,44 @@
+#
+#  ToolbarController.rb
+#  Kari
+#
+#  Created by Eloy Duran on 7/2/07.
+#  Copyright (c) 2007 __MyCompanyName__. All rights reserved.
+#
+
+require 'osx/cocoa'
+
+class ToolbarController < OSX::NSObject
+  ib_outlet :window, :historyBackAndForwardView, :fontSmallerAndBiggerView, :searchView
+  
+  def init
+    if super_init
+      @toolbar = OSX::NSToolbar.alloc.initWithIdentifier('MainWindowToolbar')
+      @toolbar.delegate = self
+      return self
+    end
+  end
+  
+  def awakeFromNib
+    @window.toolbar = @toolbar
+  end
+  
+  #TOOLBAR_ITEMS = ['HistoryBackAndForwardItem', 'FontSmallerAndBiggerItem', OSX::NSToolbarFlexibleSpaceItemIdentifier, 'SearchItem']
+  TOOLBAR_ITEMS = [OSX::NSToolbarFlexibleSpaceItemIdentifier]
+  def toolbarAllowedItemIdentifiers(toolbar)
+    TOOLBAR_ITEMS
+  end
+  def toolbarDefaultItemIdentifiers(toolbar)
+    TOOLBAR_ITEMS
+  end
+  
+  def toolbar_itemForItemIdentifier_willBeInsertedIntoToolbar(toolbar, identifier, flag)
+    item = OSX::NSToolbarItem.alloc.initWithItemIdentifier(identifier)
+    return item if identifier == OSX::NSToolbarFlexibleSpaceItemIdentifier
+    custom_view = self.instance_variable_get("@#{identifier[0...1].downcase + identifier[1..-1].gsub(/Item$/, 'View')}".to_sym)
+    item.view = custom_view
+    item.minSize = item.maxSize = OSX::NSMakeSize(OSX::NSWidth(custom_view.frame), OSX::NSHeight(custom_view.frame))
+    return item
+  end
+
+end
