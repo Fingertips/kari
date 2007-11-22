2eeb7599b305b7d7b69670cda8f4774b7131e209
eloy <eloy@7a99d2f0-d533-0410-96fe-f25cb2755c1e>
2007-11-22 12:56:08 +0000

    Fixed bug where simply trying to click a bookmark bar button but slightly moving the mouse pointer would start a drag operation. A drag operation will now only occur if the button is dragged more than 3 pixels within 1 second. These values are set in the consts: DRAG_TIMEOUT_THRESHOLD & DRAG_OFFSET_THRESHOLD
    git-svn-id: https://fngtps.com/svn/kari/trunk/src@134 7a99d2f0-d533-0410-96fe-f25cb2755c1e

--- SABookmarkBar/lib/SABookmarkButton.rb
+++ SABookmarkBar/lib/SABookmarkButton.rb
@@ -29,11 +29,45 @@ class OSX::SABookmarkButton < OSX::NSButton
       self.title = @bookmark.title
       self.sizeToFit
       
+      @dragging = @ignore_dragging = false
+      
       return self
     end
   end
   
   # drag and drop support methods
+  
+  def dragging?
+    @dragging
+  end
+  
+  def ignore_further_dragging?
+    @ignore_further_dragging
+  end
+  
+  DRAG_TIMEOUT_THRESHOLD = 1 # seconds
+  DRAG_OFFSET_THRESHOLD = 3 # pixels
+  def check_drag_threshold(offset)
+    if @started_drag_at.nil?
+      @started_drag_at = Time.now
+      time_difference = 0
+    else
+      time_difference = Time.now - @started_drag_at
+    end
+    
+    # check if the drag operation is still within the timeout
+    if time_difference < DRAG_TIMEOUT_THRESHOLD
+      # check if the drag movement is over the threshold
+      if offset.abs > DRAG_OFFSET_THRESHOLD
+        @started_drag_at = nil
+        @dragging = true
+      end
+    else
+      # did not reach drag threshold, so ignore any further drag events
+      @started_drag_at = nil
+      @ignore_further_dragging = true
+    end
+  end
   
   def mouseDown(theEvent)
     # If the button is dragged @dragging will be set to true
@@ -56,29 +90,36 @@ class OSX::SABookmarkButton < OSX::NSButton
       # FIXME: Is there a better way to call a original set action?
       self.superview.bookmarkButtonClicked(self)
     end
+    # reset
+    @dragging = @ignore_further_dragging = false
   end
   
-  def mouseDragged(theEvent)
-    @dragging = true
+  def mouseDragged(event)
+    return if ignore_further_dragging?
     
-    # What's the middle state?!
-    self.state = OSX::NSOnState
+    if not dragging?
+      # check if we really want to perform a drag
+      check_drag_threshold event.deltaX
+    else
+      # What's the middle state?!
+      self.state = OSX::NSOnState
     
-    new_x = self.frame.origin.x + theEvent.deltaX
-    if new_x < self.superview.left_margin
-      # can't move to the left beyond the superviews frame + margin
-      new_x = self.superview.left_margin
-    elsif new_x > (self.superview.frame.width - self.frame.width - self.superview.right_margin)
-      # can't move to the right beyond the superviews frame
-      new_x = self.superview.frame.width - self.frame.width - self.superview.right_margin
+      new_x = self.frame.origin.x + event.deltaX
+      if new_x < self.superview.left_margin
+        # can't move to the left beyond the superviews frame + margin
+        new_x = self.superview.left_margin
+      elsif new_x > (self.superview.frame.width - self.frame.width - self.superview.right_margin)
+        # can't move to the right beyond the superviews frame
+        new_x = self.superview.frame.width - self.frame.width - self.superview.right_margin
+      end
+    
+      self.frameOrigin = OSX::NSMakePoint(new_x, self.frame.origin.y)
+      self.needsDisplay = true
+      self.superview.needsDisplay = true
+    
+      # report position back to the bookmark bar
+      self.superview.draggingButton_xCoordinate(self)
     end
-    
-    self.frameOrigin = OSX::NSMakePoint(new_x, self.frame.origin.y)
-    self.needsDisplay = true
-    self.superview.needsDisplay = true
-    
-    # report position back to the bookmark bar
-    self.superview.draggingButton_xCoordinate(self)
   end
   
   # moving/animation
