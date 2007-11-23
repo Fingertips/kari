7b7865b41be1b6dd98eb6f5f376c9582abf82be3
eloy <eloy@7a99d2f0-d533-0410-96fe-f25cb2755c1e>
2007-11-23 11:45:10 +0000

    Check is the last ran process is still running, if so kill it. The way we do this should probably change for release.
    git-svn-id: https://fngtps.com/svn/kari/trunk/src@135 7a99d2f0-d533-0410-96fe-f25cb2755c1e

--- Backend.rb
+++ Backend.rb
@@ -1,6 +1,6 @@
 require 'osx/cocoa'
 require "socket"
-require "free_tcp_port"
+require File.expand_path("../free_tcp_port", __FILE__)
 
 class Backend < OSX::NSObject
   attr_accessor :delegate
@@ -8,6 +8,9 @@ class Backend < OSX::NSObject
   
   def init
     if super_init
+      # quit any already running process
+      kill_running_backend_process
+      
       @port = FreeTCPPort.find(:start_from => 10002)
       puts "Free TCP port found: #{@port}" if $KARI_DEBUG
       
@@ -24,6 +27,16 @@ class Backend < OSX::NSObject
     end
   end
   
+  def kill_running_backend_process
+    # this should be done through a http `quit` request instead of using kill.
+    if last_process = OSX::NSUserDefaults.standardUserDefaults['LastBackendProcess']
+      running_pid = `lsof -i tcp:#{last_process['port']} | awk '{ if ( NR > 1 ) print $2 }'`.to_i
+      if last_process['pid'] == running_pid
+        `kill #{running_pid}`
+      end
+    end
+  end
+  
   def checkIfBackendStarted(timer)
     begin
       TCPSocket.new('localhost', @port).close
@@ -36,6 +49,7 @@ class Backend < OSX::NSObject
   def launch
     OSX::NSTimer.scheduledTimerWithTimeInterval_target_selector_userInfo_repeats(0.5, self, 'checkIfBackendStarted:', nil, true)
     @backend.launch
+    OSX::NSUserDefaults.standardUserDefaults['LastBackendProcess'] = { 'port' => @port, 'pid' => @backend.processIdentifier }
   end
   
   def running?
--- spec/camp_kari_spec.rb
+++ spec/camp_kari_spec.rb
@@ -1,27 +1,50 @@
+require 'rubygems'
+require 'test/unit'
+require 'test/spec'
+require 'mocha'
+
 require File.dirname(File.expand_path(__FILE__)) + "/../Backend.rb"
 
-class Backend
-  def self.setTheReturnMock(mock)
-    @theReturnMock = mock
-  end
-  def init
-    @theReturnMock
-  end
-end
+def OSX._ignore_ns_override; true; end
 
-# describe Backend do
-#   it "should start the backend in a separate process" do
-#     backend_mock = mock("Backend")
-#     backend_mock.stub!(:launch)
-#     Backend.setTheReturnMock(backend_mock)
-#     
-#     backend = Backend.alloc.init
-#     
-#     backend.launch
-#     backend.should be_running
-#     
-#     backend.terminate
-#     sleep 2
-#     backend.should_not be_running
+# class Backend
+#   def self.setTheReturnMock(mock)
+#     @theReturnMock = mock
+#   end
+#   def init
+#     @theReturnMock
 #   end
 # end
+
+describe 'Backend' do
+  before do
+    @port = 54321
+    FreeTCPPort.stubs(:find).returns(@port)
+    @pid = 12345
+    @backend = Backend.alloc.init
+  end
+  
+  it "should start the backend in a separate process" do
+    backend = Backend.alloc
+    backend.expects(:kill_running_backend_process)
+    
+    backend.init
+  end
+  
+  it "should write the pid and port to the prefs after launching the backend process" do
+    task = @backend.instance_variable_get(:@backend)
+    task.expects(:launch)
+    task.expects(:processIdentifier).returns(12345)
+    
+    OSX::NSUserDefaults.standardUserDefaults.expects(:[]=).with('LastBackendProcess', {'port' => @port, 'pid' => @pid})
+    
+    @backend.launch
+  end
+  
+  # it "should check if the last process is still running and kill it" do
+  #   OSX::NSUserDefaults.standardUserDefaults.expects(:[]).with('LastBackendProcess').returns({'port' => @port, 'pid' => @pid})
+  #   Kernel.expects(:system).with("lsof -i tcp:#{@port} | awk '{ if ( NR > 1 ) print $2 }'").returns("#{@pid}\n")
+  #   Kernel.expects(:system).with("kill #{@pid}")
+  #   @backend.kill_running_backend_process
+  # end
+end
