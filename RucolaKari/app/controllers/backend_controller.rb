c02d9280f00446b268e094fae42fdc76c8733526
eloy <eloy@7a99d2f0-d533-0410-96fe-f25cb2755c1e>
2008-02-08 13:14:17 +0000

    Cleaned directory structure. v2
    git-svn-id: https://fngtps.com/svn/kari/trunk/src@192 7a99d2f0-d533-0410-96fe-f25cb2755c1e

--- RucolaKari/app/controllers/backend_controller.rb
+++
@@ -1,5 +0,0 @@
-class BackendController < Rucola::RCController
-  def awakeFromNib
-    puts "BackendController initialized"
-  end
-end
--- test/helpers.rb
+++
@@ -1,9 +0,0 @@
-$:.unshift(File.expand_path('../lib', File.dirname(__FILE__)))
-ENV['KARI_HOME'] = File.expand_path('fixtures', File.dirname(__FILE__))
-ENV['KARI_RI_PATH'] = File.expand_path('fixtures/ri', File.dirname(__FILE__))
-
-require 'test/unit'
-require 'active_support'
-
-require 'rubygems' rescue LoadError
-require 'mocha'
