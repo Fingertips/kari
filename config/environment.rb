a6166bbd84bec9bcf5465269fa68b236ac73adc3
eloy <eloy@7a99d2f0-d533-0410-96fe-f25cb2755c1e>
2008-12-05 08:21:51 +0000

    Turn of reloader.
    git-svn-id: https://fngtps.com/svn/kari/trunk/src@319 7a99d2f0-d533-0410-96fe-f25cb2755c1e

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,6 @@
 Rucola::Initializer.run do |config|
   # Settings specified in environment/release.rb and environment/debug.rb take precident
-  # over these settings.  
+  # over these settings.
   #
   # Load any custom Objective-C frameworks
   # config.objc_frameworks = %w(webkit quartz iokit)
