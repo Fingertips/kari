5fbfeb6e26dc0a254847aa6e2a96e86d39352118
eloy <eloy@7a99d2f0-d533-0410-96fe-f25cb2755c1e>
2008-12-16 14:28:58 +0000

    Fixied the colors on the status bar.
    git-svn-id: https://fngtps.com/svn/kari/trunk/src@388 7a99d2f0-d533-0410-96fe-f25cb2755c1e

--- app/views/status_bar.rb
+++ app/views/status_bar.rb
@@ -1,29 +1,13 @@
-class BarView < OSX::NSView
-  def initWithFrame(frame)
-    if super_initWithFrame(frame)
-      registerForWindowChangedKeyNotifications
-      self
-    end
-  end
-    
-  def registerForWindowChangedKeyNotifications
-    OSX::NSNotificationCenter.defaultCenter.addObserver_selector_name_object(self, :windowChangedKey, OSX::NSWindowDidResignKeyNotification, nil)
-    OSX::NSNotificationCenter.defaultCenter.addObserver_selector_name_object(self, :windowChangedKey, OSX::NSWindowDidBecomeKeyNotification, nil)
-  end
-  
-  def windowChangedKey(aNotification)
-    self.needsDisplay = true
-  end
-  
+class StatusBar < OSX::NSView
   def selectedBackgroundColor
     [0.588, 0.588, 0.588, 1]
   end
   
   def selectedTopLineColor
-    [0.753, 0.753, 0.753, 1]
+    [0.78, 0.78, 0.78, 1]
   end
   
-  def selectedBottomLineColor
+  def selectedSeparatorLineColor
     [0.251, 0.251, 0.251, 1]
   end
   
@@ -32,61 +16,62 @@ class BarView < OSX::NSView
   end
   
   def topLineColor
-    [0.886, 0.886, 0.886, 1]
+    [0.902, 0.902, 0.902, 1]
   end
   
-  def bottomLineColor
+  def separatorLineColor
     [0.529, 0.529, 0.529, 1]
+  end
+  
+  # --------------------------
+  
+  def initWithFrame(frame)
+    if super_initWithFrame(frame)
+      [OSX::NSWindowDidResignKeyNotification, OSX::NSWindowDidBecomeKeyNotification].each do |name|
+        OSX::NSNotificationCenter.defaultCenter.addObserver_selector_name_object(self, :windowChangedKey, name, nil)
+      end
+      self
+    end
+  end
+  
+  def windowChangedKey(aNotification)
+    self.needsDisplay = true
   end
   
   def barColors
     if window.mainWindow?
-      [selectedBackgroundColor, selectedTopLineColor, selectedBottomLineColor]
+      [selectedSeparatorLineColor, selectedTopLineColor, selectedBackgroundColor]
     else
-      [backgroundColor, topLineColor, bottomLineColor]
+      [separatorLineColor, topLineColor, backgroundColor]
     end
   end
   
   def drawRect(rect)
     @bar_height ||= bounds.size.height
     context = OSX::NSGraphicsContext.currentContext.graphicsPort
-    backgroundColor, topLineColor, bottomLineColor = barColors
+    separatorLineColor, topLineColor, backgroundColor = barColors
     
     # draw background
     OSX::CGContextSetRGBFillColor(context, *backgroundColor)
     OSX::CGContextFillRect(context, OSX::CGRect.new(rect.origin, rect.size))
     
     # white-ish top line
-    from = [rect.origin.x, @bar_height]
-    to = [rect.origin.x + rect.size.width, @bar_height]
+    from = [rect.origin.x, (@bar_height - 1)]
+    to = [rect.origin.x + rect.size.width, (@bar_height - 1)]
     drawLine(context, from, to, topLineColor)
     
-    # black-ish bottom line
-    from = [rect.origin.x, 0]
-    to = [rect.origin.x + rect.size.width, 0]
-    drawLine(context, from, to, bottomLineColor)
+    # black-ish separator line
+    from = [rect.origin.x, @bar_height]
+    to = [rect.origin.x + rect.size.width, @bar_height]
+    drawLine(context, from, to, separatorLineColor)
   end
-
+  
   def drawLine(context, from, to, color)
-    OSX::CGContextSetLineWidth(context, 2.0)
+    OSX::CGContextSetLineWidth(context, 1.0)
     OSX::CGContextBeginPath(context)
     OSX::CGContextMoveToPoint(context, *from)
     OSX::CGContextAddLineToPoint(context, *to)
     OSX::CGContextSetRGBStrokeColor(context, *color)
     OSX::CGContextStrokePath(context)
-  end
-end
-
-class StatusBar < BarView
-  def self.switch_methods(method_a, method_b)
-    class_eval do
-      alias_method "#{method_b}_original", method_b
-      alias_method method_b, method_a
-      alias_method method_a, "#{method_b}_original"
-    end
-  end
-  
-  %w{ topLineColor bottomLineColor }.each do |color|
-    switch_methods color, "selected#{color[0,1].upcase}#{color[1..-1]}"
   end
 end
