55399e3f1ff9f1f296e5343f118e5fe72c661ec0
eloy <eloy@7a99d2f0-d533-0410-96fe-f25cb2755c1e>
2008-12-17 14:13:20 +0000

    Moved the webview printing code to the web_view concern.
    git-svn-id: https://fngtps.com/svn/kari/trunk/src@395 7a99d2f0-d533-0410-96fe-f25cb2755c1e

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -106,19 +106,6 @@ class ApplicationController < Rucola::RCController
     @window.makeFirstResponder(@searchTextField)
   end
   
-  def printWebViewContents(sender)
-    print_info = OSX::NSPrintInfo.sharedPrintInfo
-    print_info.topMargin = 30
-    print_info.leftMargin = 35
-    print_info.horizontallyCentered = false
-    print_info.verticallyCentered = false
-    
-    OSX::NSPrintOperation.objc_send(
-      :printOperationWithView, @webView.mainFrame.frameView.documentView,
-                   :printInfo, print_info
-    ).runOperation
-  end
-  
   def applicationDidFinishLaunching(aNotification)
     activateSearchField
   end
