848d78c5a4e88632fedd6b1bbe288b3ba063cca7
Eloy Duran <eloy.de.enige@gmail.com>
2008-12-29 20:46:52 +0100

    Added new boot.rb file.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,13 +5,16 @@ require 'rubygems'
 require 'test/unit'
 require 'test/spec'
 require 'mocha'
-require 'rucola'
-#require 'rucola/test_helper'
-require 'rucola/test_case'
 require 'tmpdir'
 require 'fileutils'
 
-$:.push(File.expand_path('../test_helper', __FILE__))
+$: << "/Users/eloy/code/MacRuby/rucola/lib"
+require File.expand_path('../../config/boot', __FILE__)
+#require 'rucola'
+#require 'rucola/test_helper'
+require 'rucola/test_case'
+
+$: << File.expand_path('../test_helper', __FILE__)
 
 require 'dangerous_methods_override'
 require 'user_preferences'
@@ -21,14 +24,12 @@ require 'temporary_application_support_path'
 require 'fixture_helpers'
 require 'objective-c'
 
-$:.push(File.expand_path('../../lib', __FILE__))
+$: << File.expand_path('../../lib', __FILE__)
 
 require 'core_ext'
 
 ObjectiveC.require('lib/search_kit/Index', 'CoreServices')
 ObjectiveC.require('lib/search_kit/Match')
-
-require File.expand_path('../../config/boot', __FILE__)
 
 TEST_ROOT = File.expand_path(File.dirname(__FILE__))
 
