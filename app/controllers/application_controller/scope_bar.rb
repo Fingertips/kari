11b8809de179964da1d94d7e421b5f57fc98d1fe
Eloy Duran <eloy.de.enige@gmail.com>
2009-01-23 17:56:22 +0100

    Comment scope bar code.

--- app/controllers/application_controller/scope_bar.rb
+++ app/controllers/application_controller/scope_bar.rb
@@ -3,11 +3,11 @@ class ApplicationController < Rucola::RCController
   ITEM_NAME = "Name"
   
   def setup_scopeBar!
-    puts 'hier!'
-    scopeBar.delegate = self
-    
-    items = [
-      { ITEM_IDENTIFIER => "Foo", ITEM_NAME => "Bar" }
-    ]
+    # puts 'hier!'
+    # @scopeBar.delegate = self
+    # 
+    # items = [
+    #   { ITEM_IDENTIFIER => "Foo", ITEM_NAME => "Bar" }
+    # ]
   end
 end
