50c9c45f041acb248404bb4dda28c4712bf7ee3a
Manfred Stienstra <manfred@fngtps.com>
2010-02-23 11:06:35 +0100

    Make the manager tests run and silence some system output.

--- test/test_helper/temporary_application_support_path.rb
+++ test/test_helper/temporary_application_support_path.rb
@@ -1,19 +1,20 @@
 module TemporaryApplicationSupportPath
   def self.included(base)
     base.send(:before) do
-      TemporaryApplicationSupportPath.stub
+      @application_support_path = TemporaryApplicationSupportPath.stub
     end
     base.send(:after) do
-      TemporaryApplicationSupportPath.cleanup
+      TemporaryApplicationSupportPath.cleanup(@application_support_path)
     end
   end
   
   def self.stub
-    @application_support_path = File.join(Dir.tmpdir, 'kari-application-support-path')
-    Rucola::RCApp.stubs(:application_support_path).returns(@application_support_path)
+    application_support_path = File.join(Dir.tmpdir, 'kari-application-support-path')
+    Rucola::RCApp.stubs(:application_support_path).returns(application_support_path)
+    application_support_path
   end
   
-  def self.cleanup
-    FileUtils.rm_rf(@application_support_path)
+  def self.cleanup(path)
+    FileUtils.rm_rf(path)
   end
 end
