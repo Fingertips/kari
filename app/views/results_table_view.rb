c37464c5242417ba218992e882951f11f0122e8d
Manfred Stienstra <manfred@fngtps.com>
2010-02-26 11:57:26 +0100

    Go back to default styling for the result table.

--- app/views/results_table_view.rb
+++ app/views/results_table_view.rb
@@ -1,10 +1,4 @@
 class ResultsTableView < OSX::NSTableView
-  def awakeFromNib
-    self.rowHeight = 24
-    setIntercellSpacing(OSX::NSSize.new(0.0, 3.0))
-    tableColumns.first.dataCell = ResultCell.alloc.init
-  end
-  
   def _highlightColorForCell(cell)
     OSX::NSColor.secondarySelectedControlColor
   end
@@ -36,17 +30,5 @@ class ResultsTableView < OSX::NSTableView
       selectRow_byExtendingSelection(row, false) if select
       scrollRowToVisible(row)
     end
-  end
-end
-
-class ResultCell < OSX::NSCell
-  LEFT_MARGIN = 10
-  
-  def drawInteriorWithFrame_inView(frame, view)
-    objectValue.drawAtPoint(OSX::NSMakePoint(frame.origin.x + LEFT_MARGIN, frame.origin.y - 2))
-  end
-  
-  def drawsBackground
-    false
   end
 end
