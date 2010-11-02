78f6d8fc4661a6d09e80b573e21e288044a48403
Manfred Stienstra <manfred@fngtps.com>
2010-11-02 12:30:30 +0100

    Prepare the preferences controller for later use.

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -1,15 +1,15 @@
-class PreferencesController < Rucola::RCWindowController
+class PreferencesController < NSController
   def browseForRubyInstallation(sender)
-    openDirectoryPanel = OSX::NSOpenPanel.openPanel
+    openDirectoryPanel = NSOpenPanel.openPanel
     openDirectoryPanel.canChooseDirectories = true
     openDirectoryPanel.canChooseFiles = false
     openDirectoryPanel.allowsMultipleSelection = false
     openDirectoryPanel.showsHiddenFiles = true # undocumented API
     
     buttonClicked = openDirectoryPanel.runModal
-    if buttonClicked == OSX::NSOKButton
+    if buttonClicked == NSOKButton
       path = openDirectoryPanel.filenames.objectAtIndex(0).to_s
-      OSX::NSUserDefaults.standardUserDefaults.setObject_forKey(path, 'RubyInstallation')
+      NSUserDefaults.standardUserDefaults.setObject_(path, forKey: 'RubyInstallation')
     end
   end
 end
---
+++ spec/controllers/preferences_controller_spec.rb
@@ -0,0 +1,12 @@
+require File.expand_path('../../spec_helper', __FILE__)
+
+# describe 'PreferencesController' do
+#   include Controllers
+#   
+#   before do
+#     @controller = PreferencesController.alloc.init
+#   end
+#   
+#   it "browses for Ruby installaton" do
+#   end
+# end
